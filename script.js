6e82a919864317926d0c44c2f9766701455168c9
Mahrukh <70944109+mahrukh7@users.noreply.github.com>
2022-07-29 15:08:12 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var a;
+a=5;
+let red;
+const flower='rose';
+
+document.write("<b>Result:</b> ", flower);
+//using a break tag from html
+document.write("<br>");
+//paragraph and italic html tags
+document.write("<p>This is an <i>HTML</i> paragraph.</p>");
+/*printing
+the 
+value of a which is=5 */
+document.write(a);
